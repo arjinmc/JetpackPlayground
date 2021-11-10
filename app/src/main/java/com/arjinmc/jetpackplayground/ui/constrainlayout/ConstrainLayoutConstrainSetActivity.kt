867ce771c3fef38cc8f8e8799371426d1e8fb13f
arjinmc <arjinmc@hotmail.com>
2021-11-10 14:45:39 +0800

    add ConstrainLayout ConstrainSet sample

--- app/src/main/java/com/arjinmc/jetpackplayground/ui/constrainlayout/ConstrainLayoutConstrainSetActivity.kt
+++ app/src/main/java/com/arjinmc/jetpackplayground/ui/constrainlayout/ConstrainLayoutConstrainSetActivity.kt
@@ -1,16 +1,24 @@
 package com.arjinmc.jetpackplayground.ui.constrainlayout
 
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.transition.TransitionManager
+import com.arjinmc.jetpackplayground.R
 import com.arjinmc.jetpackplayground.basic.BasicActivity
-import com.arjinmc.jetpackplayground.databinding.ActConstrainLayoutBasicBinding
 import com.arjinmc.jetpackplayground.databinding.ActConstrainLayoutConstrainSetBinding
-import com.arjinmc.jetpackplayground.databinding.ActConstrainLayoutConstrainWidthBinding
 
 /**
+ * Change constrain with codes
+ * base on API 19
  * Created by Eminem Lo on 11/9/21
  * email: arjinmc@hotmail.com
  */
 class ConstrainLayoutConstrainSetActivity : BasicActivity() {
+
+    private val mOriginSet = ConstraintSet()
+    private val mChangeSet = ConstraintSet()
+
+    private var mIsChanged = false
 
     private val binding by viewBinding(ActConstrainLayoutConstrainSetBinding::inflate)
 
@@ -22,9 +30,27 @@ class ConstrainLayoutConstrainSetActivity : BasicActivity() {
 
     override fun initView() {
 
+        mOriginSet.clone(binding.layoutCl)
+
+        mChangeSet.clone(binding.layoutCl)
+        mChangeSet.connect(R.id.tv_2, ConstraintSet.LEFT, R.id.tv_3, ConstraintSet.RIGHT)
+        mChangeSet.connect(R.id.tv_3, ConstraintSet.LEFT, R.id.tv_1, ConstraintSet.RIGHT)
+        mChangeSet.centerHorizontally(R.id.tv_3, ConstraintSet.PARENT_ID)
+
+        TransitionManager.beginDelayedTransition(binding.layoutCl)
+
     }
 
     override fun initListener() {
+
+        binding.btnChange.setOnClickListener {
+            if (mIsChanged) {
+                mOriginSet.applyTo(binding.layoutCl)
+            } else {
+                mChangeSet.applyTo(binding.layoutCl)
+            }
+            mIsChanged = !mIsChanged
+        }
     }
 
     override fun initData() {
