d31de0f0a78dc9a4076a60412b214153cccdc377
arjinmc <arjinmc@hotmail.com>
2021-12-20 19:26:44 +0800

    add splash screen sample

--- app/src/main/java/com/arjinmc/jetpackplayground/SlashActivity.kt
+++ app/src/main/java/com/arjinmc/jetpackplayground/SlashActivity.kt
@@ -1,18 +1,23 @@
 package com.arjinmc.jetpackplayground
 
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.arjinmc.jetpackplayground.basic.BasicActivity
+import com.arjinmc.jetpackplayground.util.IntentUtil
 
 /**
  * Created by Eminem Lo on 7/7/21.
  * email: arjinmc@hotmail.com
  */
-class SlashActivity : BasicActivity()  {
+class SlashActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        installSplashScreen()
+        installSplashScreen()
         setContentView(R.layout.act_splash)
+
+        IntentUtil.startActivity(this, MainActivity::class.java)
+        finish()
     }
 
     override fun initView() {
