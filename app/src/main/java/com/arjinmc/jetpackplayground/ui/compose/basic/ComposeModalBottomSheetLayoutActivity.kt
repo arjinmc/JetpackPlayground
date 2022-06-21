ab2f5caa12db61ab41180515df75613357d0149d
arjinmc <arjinmc@hotmail.com>
2022-06-21 16:22:17 +0800

    optimize codes

--- app/src/main/java/com/arjinmc/jetpackplayground/ui/compose/basic/ComposeModalBottomSheetLayoutActivity.kt
+++ app/src/main/java/com/arjinmc/jetpackplayground/ui/compose/basic/ComposeModalBottomSheetLayoutActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
+import com.arjinmc.jetpackplayground.ui.compose.widget.CommonHeader
 import kotlinx.coroutines.launch
 
 /**
@@ -24,56 +25,68 @@ class ComposeModalBottomSheetLayoutActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { ModalBottomSheetLayoutPage() }
+        setContent {
+            ModalBottomSheetLayoutPage() {
+                finish()
+            }
+        }
     }
 
     @OptIn(ExperimentalMaterialApi::class)
     @Composable
-    private fun ModalBottomSheetLayoutPage() {
-        var skipHalfExpanded by remember { mutableStateOf(false) }
-        val state = rememberModalBottomSheetState(
-            initialValue = ModalBottomSheetValue.Hidden,
-            skipHalfExpanded = skipHalfExpanded
-        )
-        val scope = rememberCoroutineScope()
-        ModalBottomSheetLayout(
-            sheetState = state,
-            sheetContent = {
-                LazyColumn {
-                    items(50) {
-                        ListItem(
-                            text = { Text("Item $it") },
-                            icon = {
-                                Icon(
-                                    Icons.Default.Favorite,
-                                    contentDescription = "Localized description"
-                                )
-                            }
-                        )
+    private fun ModalBottomSheetLayoutPage(onLeftClick: () -> Unit) {
+
+        Scaffold(topBar = {
+            CommonHeader(
+                onLeftClick = onLeftClick,
+                titleStr = "Compose ModalBottomSheetLayout"
+            )
+        }) {
+            var skipHalfExpanded by remember { mutableStateOf(false) }
+            val state = rememberModalBottomSheetState(
+                initialValue = ModalBottomSheetValue.Hidden,
+                skipHalfExpanded = skipHalfExpanded
+            )
+            val scope = rememberCoroutineScope()
+            ModalBottomSheetLayout(
+                sheetState = state,
+                sheetContent = {
+                    LazyColumn {
+                        items(50) {
+                            ListItem(
+                                text = { Text("Item $it") },
+                                icon = {
+                                    Icon(
+                                        Icons.Default.Favorite,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            )
+                        }
                     }
                 }
-            }
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(
-                    Modifier.toggleable(
-                        value = skipHalfExpanded,
-                        role = Role.Checkbox,
-                        onValueChange = { checked -> skipHalfExpanded = checked }
-                    )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Checkbox(checked = skipHalfExpanded, onCheckedChange = null)
-                    Spacer(Modifier.width(16.dp))
-                    Text("Skip Half Expanded State")
-                }
-                Spacer(Modifier.height(20.dp))
-                Button(onClick = { scope.launch { state.show() } }) {
-                    Text("Click to show sheet")
+                    Row(
+                        Modifier.toggleable(
+                            value = skipHalfExpanded,
+                            role = Role.Checkbox,
+                            onValueChange = { checked -> skipHalfExpanded = checked }
+                        )
+                    ) {
+                        Checkbox(checked = skipHalfExpanded, onCheckedChange = null)
+                        Spacer(Modifier.width(16.dp))
+                        Text("Skip Half Expanded State")
+                    }
+                    Spacer(Modifier.height(20.dp))
+                    Button(onClick = { scope.launch { state.show() } }) {
+                        Text("Click to show sheet")
+                    }
                 }
             }
         }
